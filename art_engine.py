71993a5ec878b6ae3587a65c0d868bccf63ce62a
Cooper Sullivan <thecoopersully@gmail.com>
2023-03-19 16:31:27 -0500

    Organize engine, add notes

--- art_engine.py
+++ art_engine.py
@@ -4,23 +4,33 @@ import random
 from hashlib import sha1
 from PIL import Image
 
+# Configurable settings
+num_images = 44
+output_path = "output"
+renders_path = os.path.join(output_path, "renders")
+metadata_path = os.path.join(output_path, "metadata")
 
+
+# Get predefined layers as a json object from layers.config
 def load_layers_config(file_path):
     with open(file_path, "r") as f:
         layers_config = json.load(f)
     return layers_config
 
 
+# Get all png files in a given folder
 def get_elements(layer_path):
     return [file for file in os.listdir(layer_path) if file.lower().endswith('.png')]
 
 
+# Generate SHA-1 hash string from a list of attributes
 def generate_dna(attributes):
     dna_string = "".join([attr["name"] + attr["element"] for attr in attributes])
     dna_hash = sha1(dna_string.encode()).hexdigest()
     return dna_hash
 
 
+# Determine how many unique tokens can be created from the given layers
 def calculate_unique_variations(layers):
     variations = 1
     for layer in layers:
@@ -29,7 +39,8 @@ def calculate_unique_variations(layers):
     return variations
 
 
-def generate_art(layers, output_path, num_images):
+# Generate tokens with the current configurations
+def generate_art(layers):
     unique_variations = calculate_unique_variations(layers)
     if unique_variations < num_images:
         print(f"Warning: Requested {num_images} images, but only {unique_variations} unique variations are possible.")
@@ -60,8 +71,6 @@ def generate_art(layers, output_path, num_images):
                 generated_dna.add(dna)
                 break
 
-        renders_path = os.path.join(output_path, "renders")
-        metadata_path = os.path.join(output_path, "metadata")
         os.makedirs(renders_path, exist_ok=True)
         os.makedirs(metadata_path, exist_ok=True)
         composite.save(os.path.join(renders_path, f"output_{i + 1}.png"))
@@ -72,7 +81,6 @@ def generate_art(layers, output_path, num_images):
             json.dump(metadata, f, indent=2)
 
 
-layers = load_layers_config("./config/layers.json")
-output_path = "output"
-num_images = 44
-generate_art(layers, output_path, num_images)
+if __name__ == "__main__":
+    layers_config = load_layers_config("./config/layers.json")
+    generate_art(layers_config)
